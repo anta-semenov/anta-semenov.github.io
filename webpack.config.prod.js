afebf85c3c6bbc558213072c78664fcc075e9bda
Anton Semenov <anton@anton.local>
2016-09-07 17:52:53 +0500

    update prod config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -54,7 +54,7 @@ module.exports = {
       },
       {
         test: /\.(jpg|png|gif|eot|svg|ttf|woff|woff2)(\?.*)?$/,
-        include: [path.join(__dirname, 'src')],
+        include: [path.join(__dirname, 'src'), path.join(__dirname, 'node_modules')],
         loader: 'file',
         query: {name: 'images/[name].[ext]'}
       },
@@ -70,7 +70,7 @@ module.exports = {
       {
         test: /\.less/,
         loader: ExtractTextPlugin.extract('css!postcss!less'),
-        include: path.join(__dirname, 'src')
+        include: [path.join(__dirname, 'src'), path.join(__dirname, 'node_modules')]
       }
     ]
   },
