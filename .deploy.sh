6086bf747d63a76c831dafc9388e783f569fa349
Anton Semenov <anton@anton.local>
2016-09-07 17:34:18 +0500

    edit deploy.sh

--- .deploy.sh
+++ .deploy.sh
@@ -15,4 +15,4 @@ git commit -m "Deploy to GitHub Pages"
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 # tokens GH_TOKEN and GH_REF will be provided as Travis CI environment variables
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" > /dev/null 2>&1
